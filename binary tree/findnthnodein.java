2a3ff9679594d934b13bfb4e87c4d6b0feca288b
Abianusha <abianusha19500@gmail.com>
2020-03-10 12:13:15 +0530

    Add files via upload

---
+++ binary tree/findnthnodein.java
@@ -0,0 +1,80 @@
+import java.util.*;
+class node
+{
+    int data;
+    node left,right;
+    node(int val)
+    {
+        data=val;
+        left=null;
+        right=null;
+    }
+}
+class binarytree
+{
+    node root=null;
+    static int cnt=0;
+    void insert(char[] s,int key)
+    {
+        node temp=this.root;
+        int i=0;
+        node newnode=new node(key);
+        if(temp==null && s[i]=='n')
+        {
+            this.root=newnode;
+            return;
+        }
+        while(i<s.length-1)
+        {
+            if(s[i]=='L' && temp.left!=null)
+            temp=temp.left;
+            else if(s[i]=='R' && temp.right!=null)
+            temp=temp.right;
+            i+=1;
+        }
+        if(s[s.length-1]=='L')
+        {
+            temp.left=newnode;
+        }
+        else if(s[s.length-1]=='R')
+        temp.right=newnode;
+    }
+    
+    void findnthnode(node root,int n)
+    {
+        if(root!=null)
+        {
+            if(cnt<=n)
+            {
+        findnthnode(root.left,n);
+        cnt++;
+        if(cnt==n)
+        {
+          System.out.println(root.data);
+        }
+        
+        findnthnode(root.right,n);
+        }
+        }
+        
+    }
+}
+public class findnthnodein
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		char arr[]=new char[n];
+		binarytree bt=new binarytree();
+		for(int i=0;i<n;i++)
+		{
+		String s=sc.next();
+		arr=s.toCharArray();
+		System.out.println("node");
+		int j=sc.nextInt();
+		bt.insert(arr,j);
+		}
+		int sn=sc.nextInt();
+	    bt.findnthnode(bt.root,sn);
+	}
+}
